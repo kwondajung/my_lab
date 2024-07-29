38aa80fedf0f8b08636fd90f887e8e82d6005ab2
dajung <kwonkkan@naver.com>
2024-07-29 15:34:05 +0900

    top 버튼 추가

--- movies.js
+++ movies.js
@@ -105,3 +105,23 @@ function createMovieCard2(movie2) {
     return card2;
 }
 
+// top 버튼
+
+$(document).ready(function() { 
+
+    // Top 버튼 특정 스크롤높이에서만 보이기 / 숨기기
+    $(window).scroll(function(){
+    	if($(this).scrollTop() > 100){
+        	$('#top-btn').fadeIn();
+        }else{
+        	$('#top-btn').fadeOut();
+        }
+    });
+    
+    // Top 버튼 클릭시 페이지 상단으로 이동
+   	$('#top-btn').click(function (){
+    	$('body').animate({scrollTop : 0}, 800);
+        return false;
+    });
+    
+ });
